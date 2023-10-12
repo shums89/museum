d50392df3cc2eb274754588d54183e59bccb4669
Aleksandr Shumatbaev <sh889@ya.ru>
2023-10-12 21:18:39 +0500

    feat: add player

--- src/js/files/script.js
+++ src/js/files/script.js
@@ -19,14 +19,6 @@ window.addEventListener('load', () => {
   }
 });
 
-if (document.querySelector('.filter-catalog__title')) {
-  document.querySelector('.filter-catalog__title').addEventListener('click', () => {
-    if (window.innerWidth < 992) {
-      document.querySelector('.filter-catalog__items').classList.toggle('_active');
-    }
-  });
-}
-
 window.addEventListener("DOMContentLoaded", function () {
   [].forEach.call(document.querySelectorAll('input[type="tel"]'), function (input) {
     var keyCode;
@@ -125,3 +117,154 @@ function main() {
 }
 
 main();
+
+//====================================================================
+
+const videoSlider = document.querySelector('.video-slider');
+const videoPlayToggle = videoSlider.querySelector('.slide-controls__play-toggle')
+const videoProgressScale = videoSlider.querySelector('.slide-controls__progress-scale');
+const videoVolumeToggle = videoSlider.querySelector('.slide-controls__volume-toggle');
+const videoVolumeScale = videoSlider.querySelector('.slide-controls__volume-scale');
+const videoFullscreenToggle = videoSlider.querySelector('.slide-controls__fullscreen-toggle');
+let videoPlayer;
+let videoPlayerVolumeValue;
+
+const getGradient = (value) => `linear-gradient(to right, #710707 0%, #710707 ${value}%, #c4c4c4 ${value}%, #c4c4c4 100%)`;
+
+const updateControls = (control, player = videoPlayer) => {
+  if (!player) {
+    return;
+  }
+
+  let progressValue = 0;
+  let volumeValue = 0;
+
+  switch (control) {
+    case 'playIcon':
+      videoPlayToggle.dataset.toggle = player.paused ? 'play' : 'pause';
+
+    case 'volume':
+      volumeValue = Math.round(player.volume * 100);
+      videoVolumeScale.value = volumeValue;
+      videoVolumeScale.style.background = getGradient(volumeValue);
+      videoVolumeToggle.dataset.toggle = volumeValue == 0 ? 'mute' : 'volume';
+      break;
+
+    case 'progress':
+      progressValue = player.currentTime / player.duration * 100;
+      videoProgressScale.value = progressValue;
+      videoProgressScale.style.background = getGradient(progressValue);
+      break;
+
+    case 'all':
+      videoPlayToggle.dataset.toggle = player.paused ? 'play' : 'pause';
+      volumeValue = Math.round(player.volume * 100);
+      videoVolumeScale.value = volumeValue;
+      videoVolumeScale.style.background = getGradient(volumeValue);
+      videoVolumeToggle.dataset.toggle = volumeValue == 0 ? 'mute' : 'volume';
+      progressValue = player.currentTime / player.duration * 100;
+      videoProgressScale.value = progressValue;
+      videoProgressScale.style.background = getGradient(progressValue);
+  }
+};
+
+const updateVideoPlayer = (control) => {
+  if (!videoPlayer) {
+    return;
+  }
+
+  switch (control) {
+    case 'volume':
+      videoPlayer.volume = videoVolumeScale.value / 100;
+      updateControls('volume');
+      break;
+
+    case 'fullscreen':
+      videoPlayer.controls = document.fullscreen;
+      break;
+
+    case 'progress':
+      videoPlayer.currentTime = videoProgressScale.value * videoPlayer.duration / 100;
+      break;
+  }
+};
+
+const addListener = () => {
+  videoPlayer.addEventListener('volumechange', () => {
+    updateControls('volume');
+  });
+
+  videoPlayer.addEventListener('fullscreenchange', () => {
+    updateVideoPlayer('fullscreen');
+  });
+
+  videoPlayer.addEventListener('timeupdate', () => {
+    updateControls('progress');
+  });
+
+  videoProgressScale.addEventListener('input', () => {
+    updateVideoPlayer('progress');
+  });
+};
+
+const getPlayerActive = () => {
+  const videoPlayerNew = videoSlider.querySelector('.swiper-slide-active video');
+
+  if (!videoPlayerNew) {
+    return;
+  }
+
+  if (videoPlayer && videoPlayer != videoPlayerNew) {
+    videoPlayer.pause();
+    videoSlider.dataset.play = '';
+    videoPlayer = videoPlayerNew
+    updateControls('all');
+  }
+
+  videoPlayer = videoPlayerNew;
+  addListener();
+};
+
+const changeVolume = () => {
+  updateVideoPlayer('volume');
+};
+
+videoSlider.addEventListener('click', e => {
+  const target = e.target;
+
+  e.preventDefault();
+  getPlayerActive();
+
+  if (target.closest('.video-slider-view__slide') || target.closest('.slide-controls__play-toggle')) {
+    if (videoPlayer.paused) {
+      videoPlayer.play();
+      videoSlider.dataset.play = 'play';
+    } else {
+      videoPlayer.pause();
+      videoSlider.dataset.play = '';
+    }
+  }
+
+  if (target.closest('.slide-controls__volume-toggle')) {
+    if (videoVolumeToggle.dataset.toggle === 'volume') {
+      videoPlayerVolumeValue = videoVolumeScale.value / 100;
+      videoPlayer.volume = 0;
+      videoVolumeScale.value = 0;
+    } else {
+      videoPlayer.volume = videoPlayerVolumeValue || 0;
+      updateControls('volume');
+    }
+    updateControls('volume');
+  }
+
+  if (target.closest('.slide-controls__fullscreen-toggle')) {
+    videoPlayer.requestFullscreen();
+  }
+
+  updateControls('playIcon');
+});
+
+videoVolumeScale.addEventListener('input', changeVolume);
+
+getPlayerActive();
+changeVolume();
--- src/js/files/sliders.js
+++ src/js/files/sliders.js
@@ -7,7 +7,7 @@
 // При необходимости подключаем дополнительные модули слайдера, указывая их в {} через запятую
 // Пример: { Navigation, Autoplay }
 import Swiper from 'swiper';
-import { Navigation, Pagination, Autoplay } from 'swiper/modules';
+import { Navigation, Pagination, Autoplay, Thumbs } from 'swiper/modules';
 /*
 Основниые модули слайдера:
 Navigation, Pagination, Autoplay, EffectFade, Lazy, Manipulation
@@ -65,7 +65,6 @@ function initSliders() {
       preloadImages: false,
       lazy: true,
 
-
       // Эффекты
       effect: 'fade',
       autoplay: {
@@ -73,14 +72,11 @@ function initSliders() {
         disableOnInteraction: false,
       },
 
-
       // Пагинация
-
       pagination: {
         el: '.welcome-slider-controll__pagination',
         clickable: true,
       },
-
 
       // Скроллбар
       /*
@@ -132,6 +128,158 @@ function initSliders() {
           const currentSlide = document.querySelector('.welcome-slider__slider').querySelector('.fraction-controll__current');
           currentSlide.innerHTML = `0${swiper.realIndex + 1}`.slice(-2);
         }
+      }
+    });
+  }
+
+  if (document.querySelector('.video-slider-view')) { // Указываем класс нужного слайдера
+    // Создаем слайдер
+    new Swiper('.video-slider-view', { // Указываем класс нужного слайдера
+      // Подключаем модули слайдера
+      // для конкретного случая
+      modules: [Navigation, Pagination, Autoplay, Thumbs],
+      observer: true,
+      observeParents: true,
+      slidesPerView: 1,
+      spaceBetween: 0,
+      autoHeight: true,
+      speed: 800,
+
+      //touchRatio: 0,
+      //simulateTouch: false,
+      loop: true,
+      preloadImages: false,
+      lazy: true,
+
+      thumbs: {
+        swiper: '.video-slider-preview'
+      },
+
+      // Эффекты
+      // effect: 'fade',
+      // autoplay: {
+      //   delay: 3000,
+      //   disableOnInteraction: false,
+      // },
+
+      // Пагинация
+      pagination: {
+        el: '.video-slider-controll__pagination',
+        clickable: true,
+      },
+
+      // Скроллбар
+      /*
+      scrollbar: {
+        el: '.swiper-scrollbar',
+        draggable: true,
+      },
+      */
+
+      // Кнопки "влево/вправо"
+      navigation: {
+        prevEl: '.video-slider-controll__prev',
+        nextEl: '.video-slider-controll__next',
+      },
+
+      // Брейкпоинты
+      /*
+      breakpoints: {
+        320: {
+          slidesPerView: 1,
+          spaceBetween: 0,
+          autoHeight: true,
+        },
+        768: {
+          slidesPerView: 2,
+          spaceBetween: 20,
+        },
+        992: {
+          slidesPerView: 3,
+          spaceBetween: 20,
+        },
+        1268: {
+          slidesPerView: 4,
+          spaceBetween: 30,
+        },
+      },
+      */
+      // События
+      on: {
+      }
+    });
+  }
+
+  if (document.querySelector('.video-slider-preview')) { // Указываем класс нужного слайдера
+    // Создаем слайдер
+    new Swiper('.video-slider-preview', { // Указываем класс нужного слайдера
+      // Подключаем модули слайдера
+      // для конкретного случая
+      modules: [],
+      observer: true,
+      observeParents: true,
+      slidesPerView: 3,
+      spaceBetween: 0,
+      // autoHeight: true,
+      // speed: 800,
+
+      //touchRatio: 0,
+      //simulateTouch: false,
+      // loop: true,
+      // preloadImages: false,
+      lazy: true,
+
+      // Эффекты
+      // effect: 'fade',
+      // autoplay: {
+      //   delay: 3000,
+      //   disableOnInteraction: false,
+      // },
+
+      // Пагинация
+      // pagination: {
+      //   el: '.video-slider-controll__pagination',
+      //   clickable: true,
+      // },
+
+      // Скроллбар
+      /*
+      scrollbar: {
+        el: '.swiper-scrollbar',
+        draggable: true,
+      },
+      */
+
+      // Кнопки "влево/вправо"
+      // navigation: {
+      //   prevEl: '.video-buttons-control__prev',
+      //   nextEl: '.video-buttons-control__next',
+      // },
+
+      // Брейкпоинты
+      /*
+      breakpoints: {
+        320: {
+          slidesPerView: 1,
+          spaceBetween: 0,
+          autoHeight: true,
+        },
+        768: {
+          slidesPerView: 2,
+          spaceBetween: 20,
+        },
+        992: {
+          slidesPerView: 3,
+          spaceBetween: 20,
+        },
+        1268: {
+          slidesPerView: 4,
+          spaceBetween: 30,
+        },
+      },
+      */
+      // События
+      on: {
       }
     });
   }
