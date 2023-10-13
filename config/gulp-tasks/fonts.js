b0fb52f3911b153fe680ef8e671016e90cd172ff
Aleksandr Shumatbaev <sh889@ya.ru>
2023-10-13 23:29:43 +0500

    fix: update gulp for fonts

--- config/gulp-tasks/fonts.js
+++ config/gulp-tasks/fonts.js
@@ -1,6 +1,7 @@
 import fs from 'fs';
 import fonter from 'gulp-fonter-fix';
 import ttf2woff2 from 'gulp-ttf2woff2';
+import { getFontWeight, sortFonts } from './functions.js';
 
 export const otfToTtf = () => {
   // Ищем файлы шрифтов .otf
@@ -59,32 +60,14 @@ export const fonstStyle = () => {
         // Если файла нет, создаем его
         fs.writeFile(fontsFile, '', cb);
         let newFileOnly;
+        fontsFiles = fontsFiles.sort(sortFonts);
         for (var i = 0; i < fontsFiles.length; i++) {
           // Записываем подключения шрифтов в файл стилей
           let fontFileName = fontsFiles[i].split('.')[0];
           if (newFileOnly !== fontFileName) {
             let fontName = fontFileName.split('-')[0] ? fontFileName.split('-')[0] : fontFileName;
-            let fontWeight = fontFileName.split('-')[1] ? fontFileName.split('-')[1] : fontFileName;
-            if (fontWeight.toLowerCase() === 'thin') {
-              fontWeight = 100;
-            } else if (fontWeight.toLowerCase() === 'extralight') {
-              fontWeight = 200;
-            } else if (fontWeight.toLowerCase() === 'light') {
-              fontWeight = 300;
-            } else if (fontWeight.toLowerCase() === 'medium') {
-              fontWeight = 500;
-            } else if (fontWeight.toLowerCase() === 'semibold') {
-              fontWeight = 600;
-            } else if (fontWeight.toLowerCase() === 'bold') {
-              fontWeight = 700;
-            } else if (fontWeight.toLowerCase() === 'extrabold' || fontWeight.toLowerCase() === 'heavy') {
-              fontWeight = 800;
-            } else if (fontWeight.toLowerCase() === 'black') {
-              fontWeight = 900;
-            } else {
-              fontWeight = 400;
-            }
-            // fs.appendFile(fontsFile, `@font-face {\n\tfont-family: ${fontName};\n\tfont-display: swap;\n\tsrc: url("../fonts/${fontFileName}.woff2") format("woff2"), url("../fonts/${fontFileName}.woff") format("woff");\n\tfont-weight: ${fontWeight};\n\tfont-style: normal;\n}\r\n`, cb);
+            let fontWeight = getFontWeight(fontFileName, false);
+
             fs.appendFile(fontsFile, `@font-face {\n\tfont-family: '${fontName}';\n\tfont-display: swap;\n\tsrc: url('../fonts/${fontFileName}.woff2') format('woff2');\n\tfont-weight: ${fontWeight};\n\tfont-style: normal;\n}\r\n`, cb);
             newFileOnly = fontFileName;
           }
@@ -92,7 +75,6 @@ export const fonstStyle = () => {
       } else {
         // Если файл есть, выводим сообщение
         console.log("Файл scss/fonts/fonts.scss уже существует. Для обновления файла нужно его удалить!");
-
       }
     } else {
       // Если шрифтов нет
