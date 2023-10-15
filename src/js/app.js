fa3b63f52158e83011f040d312e4fe0fe761208a
Aleksandr Shumatbaev <sh889@ya.ru>
2023-10-15 14:38:22 +0500

    feat: add tickets

--- src/js/app.js
+++ src/js/app.js
@@ -76,7 +76,7 @@ import * as flsForms from "./files/forms/forms.js";
 // flsForms.formSubmit();
 
 /* Модуль формы "колличество" */
-// flsForms.formQuantity();
+flsForms.formQuantity();
 
 /* Модуль звездного рейтинга */
 // flsForms.formRating();
